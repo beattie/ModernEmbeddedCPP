d9579b4dbd8fde82a8a89fc411c00432d0816f65
Brian Beattie <beattie@beattie-home.net>
2024-03-11 10:38:32 -0700

    Reformat

--- EmptyPOSIX/src/main.cpp
+++ EmptyPOSIX/src/main.cpp
@@ -26,7 +26,6 @@
  */
 
 // ----------------------------------------------------------------------------
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "diag/trace.h"
@@ -51,17 +50,15 @@
 #pragma GCC diagnostic ignored "-Wmissing-declarations"
 #pragma GCC diagnostic ignored "-Wreturn-type"
 
-int
-main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-  // At this stage the system clock should have already been configured
-  // at high speed.
+	// At this stage the system clock should have already been configured
+	// at high speed.
 
-  // Infinite loop
-  while (1)
-    {
-       // Add your code here.
-    }
+	// Infinite loop
+	while (1) {
+		// Add your code here.
+	}
 }
 
 #pragma GCC diagnostic pop
